3160f137654e4d5c358117836fa8dc6a6edc353d
juan <juan.ferreira12@etec.sp.gov.br>
2024-09-20 17:20:43 -0300

    aula 20/09

--- my-first-spa/src/App.vue
+++ my-first-spa/src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <NavPage></NavPage>
   <ContentPage></ContentPage>
+  <LoopComponent></LoopComponent>
   <FooterPage></FooterPage>
+  <ObjetoComponent></ObjetoComponent>
 </template>
 
 <script>
   import NavPage from './components/default/NavPage.vue';
   import ContentPage from './components/default/ContentPage.vue';
   import FooterPage from './components/default/FooterPage.vue';
+  import LoopComponent from './components/default/LoopComponent.vue';
+  import ObjetoComponent from './components/default/ObjetoComponent.vue';
 
   export default{
     name: 'App',
     components:{
-      NavPage, ContentPage, FooterPage
+      NavPage, ContentPage, FooterPage, LoopComponent,ObjetoComponent
     }
   }
 </script>
