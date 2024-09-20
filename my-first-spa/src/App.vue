f169ffdfbfe629af5c9a9fabf4b960c8b7d8f089
fabio <fabio.andrade46@etec.sp.gov.br>
2024-09-20 16:03:55 -0300

    feat: Vitor

--- my-first-spa/src/App.vue
+++ my-first-spa/src/App.vue
@@ -2,17 +2,19 @@
   <NavPage></NavPage>
   <ContentPage></ContentPage>
   <FooterPage></FooterPage>
+  <LoopComponent></LoopComponent>
 </template>
 
 <script>
   import NavPage from './components/default/NavPage.vue';
   import ContentPage from './components/default/ContentPage.vue';
   import FooterPage from './components/default/FooterPage.vue';
+  import LoopComponent from './components/LoopComponent.vue';
 
   export default{
     name: 'App',
     components:{
-      NavPage, ContentPage, FooterPage
+      NavPage, ContentPage, FooterPage, LoopComponent
     }
   }
 </script>
